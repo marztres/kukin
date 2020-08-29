6b026b9dfed497c57a400f09bbac5d9785d47af4
Marcel Solera <marztres@gmail.com>
2020-08-29 18:55:15 -0500

    Configure Api controller and swagger Docs

---
+++ kukinapp/kukin/Controllers/RecipeController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace kukin.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RecipeController : ControllerBase
+    {
+
+        [HttpGet("{recipeId}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> Get(string recipeId) {
+            List<string> recipes = new List<string>() {
+                "Recipe 1",
+                "Recipe 2",
+            };
+
+            return Ok(await Task.FromResult(recipes));
+        }
+    }
+}
--- kukinapp/kukin/Startup.cs
+++ kukinapp/kukin/Startup.cs
@@ -1,10 +1,5 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -16,6 +11,11 @@ namespace kukin
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllers()
+                    .ConfigureApiBehaviorOptions(config => {
+                        config.SuppressMapClientErrors = true;
+                    });
+            services.AddSwaggerGen();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -28,12 +28,19 @@ namespace kukin
 
             app.UseRouting();
 
+            app.UseSwagger();
+
+            app.UseSwaggerUI( config => { 
+                config.SwaggerEndpoint("/swagger/v1/swagger.json","Kukin API V1");
+                config.RoutePrefix = string.Empty;
+            });
+
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Deployed in azure app service!");
-                });
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller}/{action=Index}/{id?}"
+                    );
             });
         }
     }
