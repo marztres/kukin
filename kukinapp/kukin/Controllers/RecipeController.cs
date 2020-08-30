b94de73ca06c597a54d9cf8ceb0f1b0a7cf2c4d0
Marcel Solera <marztres@gmail.com>
2020-08-30 14:33:27 -0500

    Testing AppSetting.json Sustitution

--- kukinapp/kukin/Controllers/RecipeController.cs
+++ kukinapp/kukin/Controllers/RecipeController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
@@ -11,19 +12,33 @@ namespace kukin.Controllers
     public class RecipeController : ControllerBase
     {
         private IWebHostEnvironment _enviroment { get; }
+        private IConfiguration _configuration { get; }
         
-        public RecipeController(IWebHostEnvironment env) {
+        public RecipeController(IWebHostEnvironment env, IConfiguration configuration) {
             _enviroment = env;
+            _configuration = configuration;
         }
 
-
-        [HttpGet("{recipeId}")]
+        [HttpGet]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
-        public async Task<IActionResult> Get(string recipeId) {
+        public async Task<IActionResult> Get() {
             
 
             return Ok(await Task.FromResult(_enviroment));
+        }
+
+        [HttpGet("config")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> Config()
+        {
+            List<string> kukinConfig = new List<string>();
+            kukinConfig.Add(_configuration["KukinConfig:version"]);
+            kukinConfig.Add(_configuration["KukinConfig:enableLog"]);
+            kukinConfig.Add(_configuration["KukinConfig:kukinSecure"]);
+
+            return Ok(await Task.FromResult(kukinConfig));
         }
     }
 }
